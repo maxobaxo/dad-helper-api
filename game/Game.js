94e7ba99880ad2df322df4f8fa1e50e2f3d4b286
Max Scher <maxobaxo@gmail.com>
2017-10-02 09:38:30 -0700

    add single game get and delete requests

--- game/Game.js
+++ game/Game.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 var GameSchema = new mongoose.Schema({
-  id: String,
   name: String,
   description: String,
   Bonus: String
--- game/GameController.js
+++ game/GameController.js
@@ -8,13 +8,12 @@ var Game = require('./Game');
 // creates new Game
 router.post('/', function(req, res) {
   Game.create({
-    id: req.body.id,
     name: req.body.name,
     description: req.body.description,
     bonus: req.body.bonus
   },
   function(err, game) {
-    if (err) return res.status(500).send('There was a problem adding the information to the database');
+    if (err) return res.status(500).send('There was a problem adding the information to the database.');
   });
 })
 
@@ -23,6 +22,23 @@ router.get('/', function(req, res) {
   Game.find({}, function(err, games) {
     if (err) return res.status(500).send('There was a problem finding the games.');
     res.status(200).send(games);
+  });
+});
+
+// gets a single game from the database
+router.get('/:id', function(req, res) {
+  Game.findById(req.params.id, function(err, game) {
+    if (err) return res.status(500).send('There was a problem finding the game.');
+    if (!game) return res.status(404).send('No game found.');
+    res.status(200).send(game);
+  })
+})
+
+// deletes a single game from the database
+router.delete('/:id', function(req, res) {
+  Game.findByIdAndRemove(req.params.id, function(err, game) {
+    if (err) return res.status(500).send('There was a problem deleting the user.');
+    res.status(200).send('Game ' + game.name + ' was deleted.');
   });
 });
 
