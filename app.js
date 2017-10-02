2e5720e5d11300eaad32108c96417fb5e143dd1b
Max Scher <maxobaxo@gmail.com>
2017-10-02 09:20:25 -0700

    add GameController to app.js

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
+var GameController = require('./game/GameController');
+app.use('/games', GameController);
+
 module.exports = app;
