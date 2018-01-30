7c147d1ea0845e1f4b63c9f40c4001a3df617a87
Max Scher <maxobaxo@gmail.com>
2018-01-29 23:03:33 -0800

    implement graphql server functionality

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var db = require('./db');
+var db = require("./db");
+var bodyParser = require("body-parser");
+var { graphqlExpress } = require("apollo-server-express");
+var { buildSchema } = require("graphql");
 
-var GameController = require('./game/GameController');
-app.use('/games', GameController);
-var SkillController = require('./skill/SkillController');
-app.use('/skills', SkillController);
-var AgeController = require('./age/AgeController');
-app.use('/ages', AgeController);
+// construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// the root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello World!";
+  }
+};
+
+// bodyParser is needed just for POST.
+app.use(
+  "/graphql",
+  bodyParser.json(),
+  graphqlExpress({ schema: schema, rootValue: root })
+);
+
+var GameController = require("./game/GameController");
+app.use("/games", GameController);
+var SkillController = require("./skill/SkillController");
+app.use("/skills", SkillController);
+var AgeController = require("./age/AgeController");
+app.use("/ages", AgeController);
 
 module.exports = app;
