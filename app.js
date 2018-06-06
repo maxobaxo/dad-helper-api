c0fe34a5c3bed84120a0a34394176457134ca089
Max Scher <maxobaxo@gmail.com>
2018-06-06 15:50:13 -0700

    allow cors in express instance for api

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var db = require('./db');
+var db = require("./db");
 
-var GameController = require('./game/GameController');
-app.use('/games', GameController);
-var SkillController = require('./skill/SkillController');
-app.use('/skills', SkillController);
-var AgeController = require('./age/AgeController');
-app.use('/ages', AgeController);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+var GameController = require("./game/GameController");
+app.use("/games", GameController);
+var SkillController = require("./skill/SkillController");
+app.use("/skills", SkillController);
+var AgeController = require("./age/AgeController");
+app.use("/ages", AgeController);
 
 module.exports = app;
