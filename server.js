af43af272ac34b8517cdba73c37f6bfaf081cd3a
Max Scher <maxobaxo@gmail.com>
2018-06-06 22:25:08 -0700

    add http protocol back in

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var app = require("./app");
+var http = require("http");
 var port = process.env.PORT || 3000;
 
 app.set("port", port);
 
-var server = https.createServer(app);
+var server = http.createServer(app);
 server.listen(port, function() {
   console.log("Express server listening on port " + port);
 });
