5595a238bf956f246e4302e27ed97946e88a0c4a
Max Scher <maxobaxo@gmail.com>
2018-06-06 20:28:36 -0700

    add http support for openode deployment

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var app = require('./app');
+var app = require("./app");
+var http = require("http");
 var port = process.env.PORT || 3000;
 
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
+app.set("port", port);
+
+var server = http.createServer(app);
+server.listen(port, function() {
+  console.log("Express server listening on port " + port);
 });
