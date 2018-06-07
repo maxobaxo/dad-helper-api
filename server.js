4ea7498b4377bff1e1ce45f63db7ffa752d8352e
Max Scher <maxobaxo@gmail.com>
2018-06-06 21:12:23 -0700

    add options for new https server

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var app = require("./app");
+var fs = require("fs");
 var https = require("https");
 var port = process.env.PORT || 3000;
 
+var key = fs.readFileSync("encryption/private.key");
+var cert = fs.readFileSync("encryption/primary.crt");
+var ca = fs.readFileSync("encryption/intermediate.crt");
+
+var options = {
+  key: key,
+  cert: cert,
+  ca: ca
+};
+
 app.set("port", port);
 
-var server = https.createServer(app);
+var server = https.createServer(options, app);
 server.listen(port, function() {
   console.log("Express server listening on port " + port);
 });
