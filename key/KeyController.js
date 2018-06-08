18a79f0f97a98dd757422840ee03011ae4639a05
Max Scher <maxobaxo@gmail.com>
2018-06-08 11:35:17 -0700

    implement fs.readFileSync for keys route

--- key/KeyController.js
+++ key/KeyController.js
@@ -1,11 +1,12 @@
 var express = require("express");
+var fs = require("fs");
 var router = express.Router();
 var bodyParser = require("body-parser");
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
-var key = require("/account-key.txt");
-var cert = require("/domain-csr.txt");
+var key = fs.readFileSync("./account-key.txt");
+var cert = fs.readFileSync("./domain-csr.txt");
 
 router.get("/", function(res, err) {
   if (err) return res.status(500).send("There was a problem return the keys.");
