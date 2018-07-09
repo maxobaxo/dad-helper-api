aaa4358fcb3df6132c459dbcec6dabef3fe63fc0
Max Scher <maxobaxo@gmail.com>
2018-07-09 11:56:08 -0700

    restore original file structure and setup

---
+++ db.js
@@ -0,0 +1,6 @@
+var mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb://maxobaxo:klr1leY7@ds147864.mlab.com:47864/dads-need-help"
+);
+
+// "mongodb://maxobaxo:klr1leY7@ds147864.mlab.com:47864/dads-need-help"
--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-var app = require("./public/app");
-var http = require("http");
+var app = require("./app");
 var port = process.env.PORT || 3000;
 
-app.set("port", port);
-
-var server = http.createServer(app);
-server.listen(port, function() {
+var server = app.listen(port, function() {
   console.log("Express server listening on port " + port);
 });
